150a6b8e7b106aae51980a8f3a6eb0c81c1191c3
Marcos Silva <marcosaurelio98@gmail.com>
2022-07-20 10:16:48 -0300

    Recebe Cep

--- src/main/java/com/br/nrCepConsumingApi/Cep.java
+++ src/main/java/com/br/nrCepConsumingApi/Cep.java
@@ -1,4 +1,16 @@
 package com.br.nrCepConsumingApi;
 
 public class Cep {
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String printCepAllInfo(){
+        return getCep();
+    }
 }
--- src/main/java/com/br/nrCepConsumingApi/NrCepConsumingApiApplication.java
+++ src/main/java/com/br/nrCepConsumingApi/NrCepConsumingApiApplication.java
@@ -1,13 +1,42 @@
 package com.br.nrCepConsumingApi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class NrCepConsumingApiApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(NrCepConsumingApiApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(NrCepConsumingApiApplication.class, args);
 	}
+	@GetMapping("/")
+	public String textoInit(){
+		return "teste de funcionamento";
+	}
+	@Bean
+	public RestTemplate restTemplate (RestTemplateBuilder builder){
+		return builder.build();
+	}
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate){
+	return args -> {
+		Cep cep = restTemplate.getForObject(
+				"https://viacep.com.br/ws/04128080/json/",Cep.class);
+		log.info(cep.printCepAllInfo());
+	};
+	}
+
+
 
 }
