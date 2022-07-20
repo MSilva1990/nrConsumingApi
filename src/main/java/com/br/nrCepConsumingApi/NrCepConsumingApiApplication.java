969b335921830f6cf71ec82fd0a9d9cb6b61f85e
Marcos Silva <marcosaurelio98@gmail.com>
2022-07-20 12:04:03 -0300

    Post

--- src/main/java/com/br/nrCepConsumingApi/NrCepConsumingApiApplication.java
+++ src/main/java/com/br/nrCepConsumingApi/NrCepConsumingApiApplication.java
@@ -8,15 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class NrCepConsumingApiApplication {
 	private static final Logger log = LoggerFactory.getLogger(NrCepConsumingApiApplication.class);
+	private String texto ="";
 	public static void main(String[] args) {
 		SpringApplication.run(NrCepConsumingApiApplication.class, args);
 	}
@@ -36,7 +35,10 @@ public class NrCepConsumingApiApplication {
 		log.info(cep.printCepAllInfo());
 	};
 	}
-
-
+	@PostMapping
+	public void listener(@RequestParam String cep){
+		texto = cep;
+		log.info((texto));
+	}
 
 }
