6e92acc7bb9cb71bd5f4829bd4db9edc456b6547
Marcos Silva <marcosaurelio98@gmail.com>
2022-07-21 19:01:53 -0300

    Data Access

---
+++ src/main/java/com/br/nrCepConsumingApi/RelationalDataAccess/Customer.java
@@ -0,0 +1,41 @@
+package com.br.nrCepConsumingApi.RelationalDataAccess;
+
+public class Customer {
+    long id;
+    String firstName,lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String toString(){
+    return String.format(
+            "Customer[id=%d, firstName='%s', lastName='%s']",
+            id, firstName, lastName);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
